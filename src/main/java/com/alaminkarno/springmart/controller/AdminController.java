f04f3ebee7cad9225a821937eb05bbae8cd3a43a
alamin-karno <amin15-1951@diu.edu.bd>
2024-04-17 22:57:15 +0600

    SpringMart: AdminController Added

---
+++ src/main/java/com/alaminkarno/springmart/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.alaminkarno.springmart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminHome(){
+        return "adminHome";
+    }
+}
